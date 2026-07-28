c9dfaf4f5869ee054e36cd846d7f2ab74f6f34ae
Srishanth <srishanth232007@gmail.com>
2026-07-28 11:23:07 +0530

    Variables - VS

---
+++ Java - VS/Variables.java
@@ -0,0 +1,26 @@
+class Variables{
+    public static void main(String[] args) {
+
+        // System.out.println(3 + 5);
+
+        // int num = 1 + 2;
+
+        // int num1 = 1, num2 = 2;
+        // int result = num1 + num2;
+
+        // System.out.println(result);
+
+        // String name = "Max";
+        // System.out.println("Ready to dive deeper into Java, " + name + " ?");
+
+        System.out.println("1" + 1);
+
+        // final int myNum = 1;
+
+        // myNum = 1;
+
+        // System.out.println(myNum);
+
+    }
+}
+
