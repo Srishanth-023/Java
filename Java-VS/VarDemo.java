2bcc966a37ff68af5a55cfb0ccf1dcff850bf478
Srishanth <srishanth232007@gmail.com>
2026-07-28 14:27:06 +0530

    var Keyword demo - VS

---
+++ Java-VS/VarDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class VarDemo{
+    public static void main(String[] args) {
+        var cars = new ArrayList<String>();
+
+        cars.add("BMW");
+        cars.add("Porsche");
+
+        System.out.println(cars);
+    }
+}
