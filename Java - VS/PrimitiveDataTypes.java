b77db23100e126258129ef2848c69a543175c51e
Srishanth <srishanth232007@gmail.com>
2026-07-28 11:23:41 +0530

    Primitive Data Types - VS

---
+++ Java - VS/PrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+
+
+class PrimitiveDataTypes{
+    public static void main(String[] args) {
+        // int integer = 128;
+        // long longInteger = 123456789;
+        // short shortInteger = 32767;
+        // byte by = 127;
+
+        // double decimalDouble = 3.14;
+        // float decimalFloat = 3.14f;
+
+        // char character = 'o';
+
+        // boolean booleanFlag = true;
+
+        // Unsigned int num = 255; // No unsigned numbers in JAVA
+
+        int numBin = 0b001;
+        int numHex = 0xAA;
+        double numExpn = 11e10;
+
+        System.out.println(numBin + " " + numHex + " " + numExpn);
+
+        char character = 'a';
+        character++;
+        System.out.println(character);
+    }
+}
