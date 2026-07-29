916d4d1856ec5a50595c1d0e7b748d463f2f33d2
Srishanth <srishanth232007@gmail.com>
2026-07-30 04:13:14 +0530

    Math & Boolean - VS

---
+++ Java-VS/MathOperationsAndBoolean.java
@@ -0,0 +1,17 @@
+class MathOperationsAndBoolean{
+    public static void main(String[] args){
+        // System.out.println(Math.PI);
+
+        // int randomNumber = (int)(Math.random() * (100 - 0 + 1)) + 1;
+        // System.out.println(randomNumber);
+
+        // Boolean
+        int myAge = 19, votingAge = 18;
+
+        if (myAge > votingAge){
+            System.out.println("Access granted");
+        } else{
+            System.out.println("Access denied");
+        }
+    }
+}
