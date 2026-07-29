041e61827b53d169f489bb9942de6d7af14b183a
Srishanth <srishanth232007@gmail.com>
2026-07-29 13:39:37 +0530

    Operators - VS

---
+++ Java-VS/Operators.java
@@ -0,0 +1,40 @@
+class Operators{
+    public static void main(String[] args) {
+        // // Arithmetic
+        // int num1 = 1;
+        // int num2 = 2;
+
+        // int result = num1 + num2; // +, -, *, /, %
+
+        // num1 = num1 + 1;
+        // num1 += 1;
+        // num1++;
+        // ++num1;
+
+        // int res = ++num1; // Inc. & fetch
+        // int res = num1++; // Fetch & inc.
+
+        // System.out.println(result);
+
+
+        // // Relational
+        // boolean result = num1 > num2; // >, <, ==, !=, <=, >=
+
+        // System.out.println(result);
+
+
+        // // Logical
+        // int a = 1, b = 2, x = 3, y = 4;
+
+        // boolean result = !(a > b && x < y || a >= 1);
+
+        // System.out.println(result);
+
+        // boolean negatedResult = !result;
+
+        // System.out.println(negatedResult);
+
+        
+
+    }
+}
