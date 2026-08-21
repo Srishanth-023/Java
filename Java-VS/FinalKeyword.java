400f0449bc5e887696c828018e1ff61d0a4231f1
Srishanth <srishanth232007@gmail.com>
2026-08-21 13:58:07 +0530

    Final Keyword - VS

---
+++ Java-VS/FinalKeyword.java
@@ -0,0 +1,17 @@
+final class A{
+    public final void show(){
+        System.out.println("By Me");
+    }
+}
+
+// class B extends A{ // No inheritance
+//     // public void show(){ // Cannot override
+//     //     System.out.println("By Me");
+//     // }
+// }
+
+public class FinalKeyword{
+    public static void main(String[] args) {
+        final double PI = 3.14; // No mods.
+    }
+}
